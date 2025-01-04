28c296679173d27e36a3130326520acd1de03c8d
Martynas Muizys <99730587+martynasmuizys@users.noreply.github.com>
2025-01-04 06:58:58 +0200

    chore: reverting build script

--- build.rs
+++ build.rs
@@ -3,9 +3,4 @@ fn main() {
     if std::path::Path::new("audio/let_it_happen.mp3").exists() {
         println!("cargo::rustc-cfg=audio_available=\"true\"");
     }
-
-    pkg_config::Config::new().probe("openssl").unwrap();
-    pkg_config::Config::new().probe("autoconf").unwrap();
-    pkg_config::Config::new().probe("autopoint").unwrap();
-    pkg_config::Config::new().probe("bison").unwrap();
 }
